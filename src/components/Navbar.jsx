54e2ca44135e865c1cebb7077f87475e6869cb01
mudasir <96320211+mud-ali@users.noreply.github.com>
2023-11-05 20:24:05 -0500

    switch navbar to be horizontal

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAddressCard } from '@fortawesome/free-regular-svg-icons'
-import { faCircleInfo, faHandshake, faI, faUserGroup } from '@fortawesome/free-solid-svg-icons'
+import { faCircleInfo, faHandshake, faHouse, faUserGroup } from '@fortawesome/free-solid-svg-icons'
 import { Tooltip } from 'react-tooltip'
 import styles from '@/styles/navbar.module.css'
 
@@ -9,6 +9,9 @@ const Navbar = () => {
     return (
         <aside className={styles.sidebar}>
             <ul>
+                <li><a href="/home" id='homeLink'>
+                    <FontAwesomeIcon icon={faHouse} />
+                </a></li>
                 <li><a href="/about" id='aboutLink'>
                     <FontAwesomeIcon icon={faCircleInfo} />
                 </a></li>
@@ -22,6 +25,9 @@ const Navbar = () => {
                     <FontAwesomeIcon icon={faAddressCard} />
                 </a></li>
             </ul>
+            <Tooltip anchorSelect='#homeLink' float place='right' style={{ fontFamily: "Helvetica, sans-serif" }}>
+                Home
+            </Tooltip>
             <Tooltip anchorSelect='#aboutLink' float place='right' style={{ fontFamily: "Helvetica, sans-serif" }}>
                 About Us
             </Tooltip>
